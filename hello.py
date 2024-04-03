6c8e0b547f0773431cfb159b43669f52ae3745bf
cysron-b <139706271+cysron-b@users.noreply.github.com>
2024-04-03 10:24:55 +0200

    python file

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to our Project Management Web Application!'
+    return send_file('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
